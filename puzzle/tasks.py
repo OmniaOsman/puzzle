720b2fefc4cd683c0e572dfac3b87575ac9e0a81
Omnia Osman <UGS.140547@ci.suez.edu.eg>
2023-09-30 16:47:09 +0300

    fix: edit task

--- puzzle/tasks.py
+++ puzzle/tasks.py
@@ -5,8 +5,9 @@ import hashlib
 from project.celery import app
 from .models import Puzzle
 
-@app.task(bind=True, ignore_result=False)
-def solve_puzzle(data, level=4):
+@shared_task
+def solve_puzzle(data, level):
+    # try to find a nounce value that makes the SHA-1 hash of the data and the nounce start with a certain number of zeros
     solution = ""
     nounce = 0
     while not solution.startswith("".join(["0" for _ in range(level)])):
@@ -16,11 +17,15 @@ def solve_puzzle(data, level=4):
     # get or create the puzzle model instance with the task id
     puzzle, created = Puzzle.objects.get_or_create(task_id=solve_puzzle.request.id)
 
-    # update the puzzle model instance with the result and other fields
+    # update the puzzle model instance with the data, level, nounce, solution, and status
     puzzle.data = data
+    puzzle.level = level
     puzzle.nounce = nounce
     puzzle.solution = solution
     puzzle.status = Puzzle.AsyncResult(solve_puzzle.request.id).state
+    
     # save the puzzle model instance to the database
     puzzle.save()
+    
+    # return the nounce and solution as a tuple
     return nounce, solution
